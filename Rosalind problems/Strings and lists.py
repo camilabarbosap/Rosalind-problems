2972d7742143edf36b7463d9581667a547db3f6b
Camila Barbosa <91954589+camilabarbosap@users.noreply.github.com>
2022-05-17 17:53:12 +0000

    Initial commit

---
+++ Rosalind problems/Strings and lists.py
@@ -0,0 +1,3 @@
+texto = 'AcXFSnGAhKHolaspis8CBDE6itbSSmJOMboABIvm6S5fuMQjZyfJOqG36rpolJjwLZsEEUApBn0T4vOmthS3qV953a0PekZXEOloFfDCHYVINEmPJf81smPSSRK0HABXHTidTmgFAtvq1collybitusO5CNNLMa81vqi3qmerED5TAbzU5.'
+
+print(texto[10:18] + ' ' + texto[141:151])
